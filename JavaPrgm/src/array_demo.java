a81f2bfc4010ffa7481dd487afc0f736d788f80f
sekha <sekha@DESKTOP-S3EAA6P>
2019-11-19 08:15:39 +0530

    second

--- JavaPrgm/src/array_demo.java
+++ JavaPrgm/src/array_demo.java
@@ -17,7 +17,7 @@ public class array_demo {
 		for(int i=0;i<2;i++)
 		{
 			for(int j=0;j<3;j++) {
-				System.out.print(arr[i][j]+" ");
+				System.out.print(" "+arr[i][j]+" ");
 			}
 		System.out.println();
 		}
