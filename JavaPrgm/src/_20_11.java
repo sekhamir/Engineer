d0370804f34fa4e4c7c808be1707dcaa329cb7b3
sekha <sekha@DESKTOP-S3EAA6P>
2019-11-20 08:20:39 +0530

    20-11-2019

---
+++ JavaPrgm/src/_20_11.java
@@ -0,0 +1,41 @@
+import java.util.ArrayList;
+
+public class _20_11 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int arr[] = new int[10];
+		arr[0]=11;
+		
+
+		ArrayList lst = new ArrayList();
+		System.out.println("Size:"+lst.size());
+		lst.add(11);
+		lst.add("Hello");
+		lst.add(23.33F);
+		System.out.println("Size:"+lst.size());
+		
+		//insert at particular index
+		lst.add(0, 200);
+		
+		lst.trimToSize();
+		
+		Object[] data = lst.toArray();
+		lst.set(2,"Hii");
+		System.out.println("Last Index of 11:"+lst.lastIndexOf(11));
+		System.out.println("Last Index of 11:"+lst.indexOf(11));
+		
+		System.out.println("get:"+lst.get(2));
+		
+		System.out.println("Contains:"+lst.contains(111));
+		
+		for (Object o:lst)
+		{
+			System.out.println(o);
+		}
+
+	
+	}
+
+}
