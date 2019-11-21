75fe242199e42cf3e1459e0bf3bcbf973da50caa
sekha <sekha@DESKTOP-S3EAA6P>
2019-11-21 20:46:54 +0530

    20-11-2019

--- JavaPrgm/src/removeRange.java
+++ JavaPrgm/src/removeRange.java
@@ -1,22 +1,22 @@
-import java.util.ArrayList;
-
-public class removeRange extends ArrayList{
-
-	public static void main(String[] args) {
-	
-		removeRange ar = new removeRange();
-		ar.add(11);
-		ar.add(22);
-		ar.add(33);
-		ar.add(44);
-		ar.add(55);
-		
-		ar.removeRange(1, 3);
-
-		for(Object e:ar)
-		{
-			System.out.println(e);
-		}
-	}
-
-}
+//import java.util.ArrayList;
+//
+//public class removeRange extends ArrayList{
+//
+//	public static void main(String[] args) {
+//	
+//		removeRange ar = new removeRange();
+//		ar.add(11);
+//		ar.add(22);
+//		ar.add(33);
+//		ar.add(44);
+//		ar.add(55);
+//		
+//		ar.removeRange(1, 3);
+//
+//		for(Object e:ar)
+//		{
+//			System.out.println(e);
+//		}
+//	}
+//
+//}
